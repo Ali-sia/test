714b12cf2e19c5bcd99d596dc0ff459ed461e942
aliesia-zahorui <aliesia@ukr.net>
2022-11-28 12:39:19 +0200

    кнопки добавления удаления в очередь
    плюс все к ним
    
    не закончено

--- src/index.js
+++ src/index.js
@@ -8,12 +8,18 @@ const refs = {
   galleryContainer: document.querySelector('.gallery'),
   searchForm: document.querySelector('.search-form'),
   galleryImage: '',
-  //   galleryImage: document.querySelector('.gallery .gallery-image'),
   formEl: document.querySelector('form'),
   modalEl: document.querySelector('.modal'),
   pageEl: document.querySelector('body'),
   btnClose: '',
+  modalFavorites: '',
+  modalShow: '',
 };
+
+const localStorageIdFav = [];
+const localStorageIdShow = [];
+let isAddedFav = false;
+let isAddedShow = false;
 
 const photoApiService = new PhotoApiService();
 
@@ -60,6 +66,7 @@ function clearPhotoContainer() {
   refs.galleryContainer.innerHTML = '';
 }
 
+//відкрити модальне вікно, підвантажити туди інфо про обране фото
 async function openModal(id) {
   console.log(' -> openModal -> id', id);
   refs.modalEl.classList.add('modal--show');
@@ -70,9 +77,58 @@ async function openModal(id) {
 
   refs.btnClose = document.querySelector('.modal__button-close');
   refs.btnClose.addEventListener('click', () => closeModal());
-}
 
+  refs.modalFavorites = document.querySelector('.modal__button-favorites');
+  refs.modalShow = document.querySelector('.modal__button-show');
+  refs.modalFavorites.addEventListener('click', () => addToFavourite(id));
+  refs.modalShow.addEventListener('click', () => addToShow(id));
+}
+//закрити модальне вікно
 function closeModal() {
   refs.modalEl.classList.remove('modal--show');
   refs.pageEl.classList.remove('stop-scrolling');
+}
+
+//додавання до списків
+function addToFavourite(id) {
+  if (isAddedFav === true) {
+    refs.modalFavorites.classList.remove('added');
+    isAddedFav = false;
+
+    console.log(`DELETE FROM addToFavourite`);
+    //якщо містить id
+    // const idFavorites = JSON.parse(localStorage.getItem('idFavorites'));
+    // const indexDelete = idFavorites.indexOf(id);
+    // console.log('🌺 -> addToFavourite -> indexDelete', indexDelete);
+    // idFavorites.splice(idFavorites, indexDelete);
+    // localStorage.removeItem('idFavorites', JSON.stringify(idFavorites));
+  } else {
+    refs.modalFavorites.classList.add('added');
+    isAddedFav = true;
+
+    console.log(`ADD TO addToFavourite`);
+    //#TODO
+    //доддати перевірку на існування такого запису
+    localStorageIdFav.push(id);
+    const serializedState = JSON.stringify(localStorageIdFav);
+    localStorage.setItem('idFavorites', serializedState);
+  }
+}
+
+function addToShow(id) {
+  //   if (!isAddedShow) {
+  //     refs.modalShow.classList.remove('added');
+  //     isAddedShow = false;
+
+  //     console.log(`DELETE FROM addToFavourite`);
+  //   }
+
+  refs.modalShow.classList.add('added');
+  //   isAddedShow = true;
+
+  console.log(`ADD TO addToShow`);
+
+  localStorageIdShow.push(id);
+  const serializedState = JSON.stringify(localStorageIdShow);
+  localStorage.setItem('idShow', serializedState);
 }
